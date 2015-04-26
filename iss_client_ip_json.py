4008bb60a3b0358a5b0e66378e7631dc44af44d0
Alejandro S <yonseca@gmail.com>
2015-04-26 18:06:17 +0000

    Not the best solution, but it won't crash

--- iss_client_ip_json.py
+++ iss_client_ip_json.py
@@ -5,13 +5,14 @@ from TwitterAPI import TwitterAPI
 import time
 import tokens
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 api = TwitterAPI(tokens.consumer_key, tokens.consumer_secret, tokens.access_token_key, tokens.access_token_secret)
+
 lat_iss = 0
 lon_iss = 0
 speed_iss = 0
 alt_iss = 0
-
 lat_cli = 0
 lon_cli = 0
 counter = 0
@@ -32,10 +33,48 @@ def iss_tracker():
 	speed_iss = response_iss['velocity']
 	alt_iss = response_iss['altitude']
 	return(lat_iss,lon_iss,speed_iss,alt_iss)
+
 def cli_tracker():	
 	lat_cli = response_client['latitude']
 	lon_cli = response_client['longitude']
 	return(lat_cli,lon_cli)
+
+def doWork():
+	
+	global lat_iss
+	global lon_iss
+	global speed_iss
+	global alt_iss
+	global lat_cli
+	global lon_cli
+	global counter
+	try:
+		lat_iss,lon_iss,speed_iss,alt_iss = iss_tracker()
+		location = geolocator.reverse([lat_iss,lon_iss])
+		print ("\nPosition of the ISS")
+		print (lat_iss)
+		print (lon_iss)
+		###raw_loc = location.raw
+		#print(raw_loc)
+		string_speed = str(speed_iss)
+		string_alt = str(alt_iss)
+		speed_short = string_speed[0:7]
+		alt_short = string_alt[0:7]
+		print (speed_short)
+		print (alt_short)
+		'''Now, with complete address of the ISS'''
+		print("\nPosition in Earth")
+		print(location.address)	
+		time.sleep(5)
+		counter = counter + 1
+		print counter
+		#if counter == 24:
+		if(location.address != None):
+			r = api.request('statuses/update', {'status':'Hi there! Now in: '+location.address+', speed: '+speed_short+', altitude: '+alt_short})
+			print r.status_code
+			counter = 0
+	except GeocoderTimedOut as e:
+		print "*BOOM*"
 
 #lat_iss,lon_iss = iss_tracker()
 lat_cli,lon_cli = cli_tracker()
@@ -44,29 +83,5 @@ print (lat_cli)
 print (lon_cli)
 
 while True:
-	lat_iss,lon_iss,speed_iss,alt_iss = iss_tracker()
-	location = geolocator.reverse([lat_iss,lon_iss])
-	print ("\nPosition of the ISS")
-	print (lat_iss)
-	print (lon_iss)
-	###raw_loc = location.raw
-	#print(raw_loc)
-	string_speed = str(speed_iss)
-	string_alt = str(alt_iss)
-	speed_short = string_speed[0:7]
-	alt_short = string_alt[0:7]
-	print (speed_short)
-	print (alt_short)
-	'''Now, with complete address of the ISS'''
-	print("\nPosition in Earth")
-	print(location.address)	
-	time.sleep(5)
-	counter = counter + 1
-	print counter
-	#if counter == 24:
-	if(location.address != None):
-		r = api.request('statuses/update', {'status':'Hi there! Now in: '+location.address+', speed: '+speed_short+', altitude: '+alt_short})
-		print r.status_code
-		counter = 0
-
+	doWork();
 
